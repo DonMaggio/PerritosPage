8cff5440a5199ba1ecfabdc9e278d2b72fb765ed
BradoAlan <alan.luis.maggio@gmail.com>
2024-10-29 13:53:26 -0300

    so modifica allowed host

--- web_perritos/settings.py
+++ web_perritos/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'bigotes-felices.onrender.com', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
